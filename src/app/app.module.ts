99e362443c8f394f1df4696143c0811632c9590b
paco124 <pacomelchor51@gmail.com>
2023-11-01 16:57:25 -0600

    First Commit 01-11-2023

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './core/layaut/navbar/navbar.component';
+import { LoginComponent } from './Ticket/login/login.component';
+import { AdminComponent } from './Ticket/admin/admin.component';
+import { UserComponent } from './Ticket/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    LoginComponent,
+    UserComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
