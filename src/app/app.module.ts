69186d4c6965b0566b790ddaec0e9738693beeda
paco124 <pacomelchor51@gmail.com>
2023-11-07 16:50:57 -0600

    FEAT:07-11-2023 change arquitcture in modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,17 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './core/layaut/navbar/navbar.component';
-import { LoginComponent } from './Ticket/login/login.component';
-import { AdminComponent } from './Ticket/admin/admin.component';
-import { UserComponent } from './Ticket/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    LoginComponent,
-    UserComponent,
-    AdminComponent
   ],
   imports: [
     BrowserModule,
